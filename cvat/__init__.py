0073e7a13417c084b86ee1518aa5c677c4aaac5f
hc0 <adding@yeah.net>
2020-11-10 11:16:24 +0800

    精简-1

--- cvat/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2018-2020 Intel Corporation
-#
-# SPDX-License-Identifier: MIT
-
-from cvat.utils.version import get_version
-
-VERSION = (1, 2, 0, 'alpha', 0)
-
-__version__ = get_version(VERSION)
