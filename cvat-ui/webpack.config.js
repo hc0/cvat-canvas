2b2dc4c29d32479548a825228e00ba0d80a93033
zhengcunjie <zhengcunjie@itangquan.com>
2020-11-21 11:16:35 +0800

    run

--- cvat-ui/webpack.config.js
+++ cvat-ui/webpack.config.js
@@ -67,10 +67,6 @@ module.exports = {
                 },
             },
             {
-                test: /\.(png|jpg|gif)$/,
-                use: ['file-loader'],
-            },
-            {
                 test: /\.(css|scss)$/,
                 use: [
                     'style-loader',
